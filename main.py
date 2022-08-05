62ebaaec5607e06e1618a388465b87ae5d7ce85f
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-08-05 09:40:26 +0000

    updated main file

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import typer
 from src.build_dataset import prepare_dataset
-from src.pytorch.dataloader import image_datasets
+# from src.pytorch.train import train_and_save_model
 
 
 def main():
-    # prepare_dataset()
+    prepare_dataset()
     # datasets
-    print(image_datasets)
+    # print(image_datasets)
+    # train model
+    # train_and_save_model()
 
 
 if __name__ == "__main__":
